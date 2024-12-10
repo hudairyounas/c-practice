ecca0928ba720023edd557e4a659c69b8b31c3f0
Hudair Younis <hudairshorts@gmail.com>
2024-12-10 23:01:20 +0500

    c language test. complete 1st chapter

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("Hello, World!");
+}
+// Write a variable with the name "a" and the value "10"
+// print this variable to the console
+// use scanf() function in your program
+// now get the value of this variable from the user and print it to the console
+// use getch() function in your program and print the value of this variable to the console
+// write a program that takes two numbers as input and prints the sum of these two numbers
+// write a program that takes two numbers as input and prints the difference of these two numbers
+// add more questions to this file
